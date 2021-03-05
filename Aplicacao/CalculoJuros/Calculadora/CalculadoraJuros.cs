7975ee527ba761e3320514817f4160e4519bf520
Fabio de Stefani <fabiostefani@gmail.com>
2021-03-05 12:41:00 -0300

    Remoção de comentários

--- Aplicacao/CalculoJuros/Calculadora/CalculadoraJuros.cs
+++ Aplicacao/CalculoJuros/Calculadora/CalculadoraJuros.cs
@@ -9,20 +9,5 @@ namespace Aplicacao.CalculoJuros.Calculadora
             var valorJuros = (double)valorInicial * Math.Pow((1 + (double)juros), meses);
             return decimal.Parse(valorJuros.ToString("0.00"));
         }
-
-        private static decimal TruncateDecimal(this decimal valor, int decimais)
-        {
-            decimal valorIntegral = Math.Truncate(valor);
-
-            decimal fracao = valor - valorIntegral;
-
-            decimal fator = (decimal)Math.Pow(10, decimais);
-
-            decimal fracaoTruncada = Math.Truncate(fracao * fator) / fator;
-
-            decimal resultado = valorIntegral + fracaoTruncada;
-
-            return resultado;
-        }
     }
 }
