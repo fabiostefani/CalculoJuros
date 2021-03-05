7ef3fa8d8aa65eb4a4180a589e25b5232dbf6f72
Fabio de Stefani <fabiostefani@gmail.com>
2021-03-05 07:12:35 -0300

    Ajustes no cálculo de Juros para 2 casas decimais

--- Aplicacao/CalculoJuros/Calculadora/CalculadoraJuros.cs
+++ Aplicacao/CalculoJuros/Calculadora/CalculadoraJuros.cs
@@ -7,8 +7,22 @@ namespace Aplicacao.CalculoJuros.Calculadora
         public static decimal Calcular(decimal valorInicial, int meses, decimal juros)
         {
             var valorJuros = (double)valorInicial * Math.Pow((1 + (double)juros), meses);
+            return decimal.Parse(valorJuros.ToString("0.00"));
+        }
 
-            return decimal.Truncate((decimal)valorJuros * 100m) / 100;
+        private static decimal TruncateDecimal(this decimal valor, int decimais)
+        {
+            decimal valorIntegral = Math.Truncate(valor);
+
+            decimal fracao = valor - valorIntegral;
+
+            decimal fator = (decimal)Math.Pow(10, decimais);
+
+            decimal fracaoTruncada = Math.Truncate(fracao * fator) / fator;
+
+            decimal resultado = valorIntegral + fracaoTruncada;
+
+            return resultado;
         }
     }
 }
